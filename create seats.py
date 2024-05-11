5031c30af16dc68d0e41049e6995808328bc7fb6
myselfvedant <myselfvedantghuge@gmail.com>
2024-05-11 23:41:09 +0530

    Update create seats.py
    Added createSeats and createSeatsLayout functions

--- create seats.py
+++ create seats.py
@@ -1,2 +1,24 @@
 class createLayout:
+    def __init__(self,numberOfRooms, seatsInEachRoom, distributionsOfSeats) :
+        self.numberOfRooms = numberOfRooms
+        self.seatsInEachRoom = seatsInEachRoom
+        self.distributionsOfSeats = distributionsOfSeats #this will be a list with touple as (columb, row) for each row
+
+    def createSeats(self,roomNumber): #create seats just by number withour taking location into consideration
+        currentRoom = roomNumber - 1
+        seatsInCurrentRoom = self.seatsInEachRoom[currentRoom]
+        return [*range(1, seatsInCurrentRoom + 1)]
+
+    def createSeatsLayout(self,roomNumber): #considering location in the table (considering the room is a square matrix)
+        currentRoom = roomNumber - 1
+        currentRoomsDistribution = self.distributionsOfSeats[currentRoom] #this will be a touple with (columb, row)
+        seats = []
+        for columb in range(currentRoomsDistribution[0]):
+            for row in range(currentRoomsDistribution[1]):
+                seats.append((columb+1,row+1))
+        return seats
+
+        
+
+
   
