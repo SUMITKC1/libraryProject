7e59add12b3b56539ab65476adb4dfc0d592d2a3
Vedant Ghuge <vedantghuge.login@gmail.com>
2024-05-12 16:52:10 +0530

    added a method to read data and then add new data

--- createDatabaseOfStudent.py
+++ createDatabaseOfStudent.py
@@ -1,6 +1,32 @@
-import pandas as pd
-df = pd.read_csv("studentDetails.csv")
-print(df)
+class DataBase:
+    def __init__(self, databaseCsv = "studentDetails.csv"):
+        self.databaseCsv = databaseCsv
+        import pandas as pd
+        self.df = pd.read_csv(databaseCsv)
+        
+
+    #this is a very rough code to make it work will need to better it.
+    def doesRecordExists(self, rollNumber):
+        data = self.df[self.df["rollNumber"] == rollNumber]
+        data = data.to_dict()
+        if data["rollNumber"] == {}:
+            return False
+        return True
+
+    def addRecord(self, name, rollNumber):
+        dataToAppend = f"\n{name},{rollNumber}"
+        with open(self.databaseCsv,mode = "a", newline="") as file:
+            file.write(dataToAppend)
+            print("Record updated successfully.")
+    
+
+
+        
+
+
+
+
+
 
 
 
